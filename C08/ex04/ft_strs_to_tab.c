e8d1d744325fa0386b0232c0bcd98f6721212f29
Sophia-PeiJin-SU <peijin.sophia.su@gmail.com>
2021-08-20 13:00:31 +0930

    C08

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+#include "ft_stock_str.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *)malloc(ft_strlen(src) * sizeof(char) + 1);
+	if (dest == NULL)
+		return (0);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+t_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int			i;
+	t_stock_str	*array;
+
+	array = malloc((ac + 1) * sizeof(t_stock_str));
+	if (array == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		array[i].size = ft_strlen(av[i]);
+		array[i].str = av[i];
+		array[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	array[i] = (t_stock_str){0, 0, 0};
+	return (array);
+}
